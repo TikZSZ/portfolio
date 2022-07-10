bb3d9c831a20f00189c3b4983f841d6c4a9ff59d
Aditya <repackergame@gmail.com>
2022-07-10 20:55:15 +0000

    vite env

--- src/baseUrl.ts
+++ src/baseUrl.ts
@@ -1,5 +1,5 @@
 //export const baseUrl = "https://protfolio-nestjs.herokuapp.com"
-export const baseUrl = import.meta.env.VUE_APP_BASE_URL ||  "http://localhost:3000"
+export const baseUrl = import.meta.env.VITE_APP_BASE_URL ||  "http://localhost:3000"
 //"https://protfolio-nestjs.herokuapp.com"
 
 // "http://localhost:3000"
